8b6a8cca1a8eab377dcfe02ce01322a6e914543d
Shashank <40920035+shashank524@users.noreply.github.com>
2023-03-22 21:35:07 -0400

    Refactored

--- model.py
+++ model.py
@@ -1,37 +1,40 @@
-from keras.layers import Conv3D,ConvLSTM2D,Conv3DTranspose
+import keras.layers as layers
 from keras.models import Sequential
 
-
-
 def load_model():
+    """
+    Returns the spatiotemporal autoencoder model for abnormal event detection in videos.
+    """
+    model = Sequential()
+    model.add(layers.Conv3D(filters=128, kernel_size=(11, 11, 1), strides=(4, 4, 1),
+                            padding='valid', input_shape=(227, 227, 10, 1), activation='tanh'))
+    model.add(layers.Conv3D(filters=64, kernel_size=(5, 5, 1), strides=(2, 2, 1),
+                            padding='valid', activation='tanh'))
 
-	"""
-	Return the model used for abnormal event 
-	detection in videos using spatiotemporal autoencoder
+    # Add ConvLSTM layers
+    convlstm_params = {
+        'filters': [64, 32, 64],
+        'kernel_size': (3, 3),
+        'strides': 1,
+        'padding': 'same',
+        'dropout': [0.4, 0.3, 0.5],
+        'return_sequences': True
+    }
+    for i in range(len(convlstm_params['filters'])):
+        model.add(layers.ConvLSTM2D(filters=convlstm_params['filters'][i],
+                                    kernel_size=convlstm_params['kernel_size'],
+                                    strides=convlstm_params['strides'],
+                                    padding=convlstm_params['padding'],
+                                    dropout=convlstm_params['dropout'][i],
+                                    return_sequences=convlstm_params['return_sequences']))
 
-	"""
-	model=Sequential()
-	model.add(Conv3D(filters=128,kernel_size=(11,11,1),strides=(4,4,1),padding='valid',input_shape=(227,227,10,1),activation='tanh'))
-	model.add(Conv3D(filters=64,kernel_size=(5,5,1),strides=(2,2,1),padding='valid',activation='tanh'))
+    # Add Conv3DTranspose layers
+    model.add(layers.Conv3DTranspose(filters=128, kernel_size=(5, 5, 1), strides=(2, 2, 1),
+                                     padding='valid', activation='tanh'))
+    model.add(layers.Conv3DTranspose(filters=1, kernel_size=(11, 11, 1), strides=(4, 4, 1),
+                                     padding='valid', activation='tanh'))
 
+    # Compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
-
-	model.add(ConvLSTM2D(filters=64,kernel_size=(3,3),strides=1,padding='same',dropout=0.4,recurrent_dropout=0.3,return_sequences=True))
-
-	
-	model.add(ConvLSTM2D(filters=32,kernel_size=(3,3),strides=1,padding='same',dropout=0.3,return_sequences=True))
-
-
-	model.add(ConvLSTM2D(filters=64,kernel_size=(3,3),strides=1,return_sequences=True, padding='same',dropout=0.5))
-
-
-
-
-	model.add(Conv3DTranspose(filters=128,kernel_size=(5,5,1),strides=(2,2,1),padding='valid',activation='tanh'))
-	model.add(Conv3DTranspose(filters=1,kernel_size=(11,11,1),strides=(4,4,1),padding='valid',activation='tanh'))
-
-	model.compile(optimizer='adam',loss='mean_squared_error',metrics=['accuracy'])
-
-	return model
-
-
+    return model
